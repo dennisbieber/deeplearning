2de522f3b0990b6a0e6b1f5e196af00b062ec56a
Dennis Bieber <de.bieber@gmail.comr>
2019-02-15 22:05:02 +0100

    Bereitet Logistische Regression (Multiclass) vor

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import numpy as np
-from neuron import Neuron
+from sklearn.preprocessing import OneHotEncoder
+
 import mnistreadlib as mr
+from neuron import Neuron
 
 xs = mr.read_mnist_images("train-images-idx3-ubyte.gz").reshape(-1, 784)
-ys = (mr.read_mnist_labels("train-labels-idx1-ubyte.gz") == 8).astype(np.float32)
+ys = mr.read_mnist_labels("train-labels-idx1-ubyte.gz")
+ys = OneHotEncoder().fit_transform(ys.reshape(-1, 1)).toarray().T
 
 xtest = mr.read_mnist_images("t10k-images-idx3-ubyte.gz").reshape(-1, 784)
-ytest = mr.read_mnist_labels("t10k-labels-idx1-ubyte.gz") == 8
+ytest = mr.read_mnist_labels("t10k-labels-idx1-ubyte.gz")
 
-n = Neuron(784, 0.00001)
+n = Neuron(784, 10, 0.00001)
 
-for k in range(0, 60000, 1000):
-
-    n.train(xs[k:k + 1000, :], ys[k:k + 1000])
+for k in range(0, 1):
+    n.train(xs, ys)
 
     cost = n.costs(xs, ys)
     pred = np.mean(((n.predict(xtest) > 0.5) == ytest).astype(np.float32))
--- neuron.py
+++ neuron.py
@@ -1,44 +1,20 @@
 import numpy as np
 from scipy.special import expit
 
-#def S(x):
-#    return expit(x)
-
-
-#def f(w, n, x):
-#    return S(w @ x.T + n)
-
-
-#def j(w, n, x, y):
-#    predict = f(w, n, x)
-#    prop_of_ones = y * np.log(predict)
-#    prop_of_zeros = (1 - y) * np.log(1 - predict)
-#    return -np.mean(prop_of_ones + prop_of_zeros)
-
-
-#def j_ableitung_w(w, n, x, y):
-#    e = f(w, n, x) - y
-#    return np.mean(x.T * e, axis=1)
-
-
-#def j_ableitung_n(w, n, x, y):
-#    e = f(w, n, x) - y
-#    return np.mean(e)
-
 class Neuron:
 
-    def __init__(self, weight_count, learn_rate=0.1):
-        self.w = np.zeros((1, weight_count), np.float32)
-        self.b = 0.0
+    def __init__(self, weight_count, output_count, learn_rate=0.1):
+        self.w = np.zeros((output_count, weight_count))
+        self.b = np.zeros(output_count)
         self.learn_rate = learn_rate
 
     def predict(self, x):
-        return expit(self.w @ x.T + self.b)
+        return expit((self.w @ x.T).T + self.b).T
 
     def train(self, x, y):
         e = self.predict(x) - y
-        dw = np.mean(x.T * e, axis=1)
-        db = np.mean(e)
+        dw = (x.T @ e.T / x.shape[0]).T
+        db = np.mean(e, axis=1)
         self.w -= self.learn_rate * dw
         self.b -= self.learn_rate * db
 
@@ -46,4 +22,4 @@ class Neuron:
         predict = self.predict(x)
         prop_of_one = y * np.log(predict)
         prop_of_zero = (1 - y) * np.log(1 - predict)
-        return -np.mean(prop_of_one + prop_of_zero)
+        return -np.mean(prop_of_one + prop_of_zero, axis=1)
