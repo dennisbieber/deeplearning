a5e4180336048ecdcf5d5ef4ba98baf88dcb51bb
Dennis Bieber <de.bieber@gmail.comr>
2019-02-25 21:04:16 +0100

    Bereitet Logistische Regression (Multiclass) vor (2)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.misc import imread
 from sklearn.preprocessing import OneHotEncoder
 
 import mnistreadlib as mr
@@ -6,20 +7,16 @@ from neuron import Neuron
 
 xs = mr.read_mnist_images("train-images-idx3-ubyte.gz").reshape(-1, 784)
 ys = mr.read_mnist_labels("train-labels-idx1-ubyte.gz")
-ys = OneHotEncoder().fit_transform(ys.reshape(-1, 1)).toarray().T
+ys = OneHotEncoder(categories='auto').fit_transform(ys.reshape(-1, 1)).toarray().T
 
-xtest = mr.read_mnist_images("t10k-images-idx3-ubyte.gz").reshape(-1, 784)
-ytest = mr.read_mnist_labels("t10k-labels-idx1-ubyte.gz")
+image = 255. - imread("9.png").reshape(1, -1)
 
-n = Neuron(784, 10, 0.00001)
+n = Neuron(784, ys.shape[0], 0.00001)
 
-for k in range(0, 1):
+for k in range(0, 50):
+
     n.train(xs, ys)
 
-    cost = n.costs(xs, ys)
-    pred = np.mean(((n.predict(xtest) > 0.5) == ytest).astype(np.float32))
-    print(k + 1000, "Vorhersagekraft:", pred, ", Kosten:", cost)
-
-
-
-
+    pred = n.predict(image)
+    num = np.argmax(pred, axis=0)
+    print(k + 1, num, "(" + str(pred.reshape(1, -1)[0][num]) + ")")
--- mnistreadlib.py
+++ mnistreadlib.py
@@ -1,5 +1,7 @@
 import gzip
+
 import numpy as np
+
 
 def read_mnist_images(name):
     with gzip.open(name, mode="rb") as f:
@@ -10,4 +12,5 @@ def read_mnist_images(name):
 
 def read_mnist_labels(name):
     with gzip.open(name, mode="rb") as f:
-        return np.frombuffer(f.read(), offset=8, dtype=np.uint8)
+        return np.frombuffer(f.read(), offset=8, dtype=np.uint8) \
+            .astype(np.float32)
